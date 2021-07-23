2357b95423176d7ce58161a5190de8965243a7dc
Senth <senth.wallace@gmail.com>
2021-07-23 18:47:44 +0200

    ♻ Now runs actions in the background ➕ tealprint, blulib

--- webhook_actions/config.py
+++ webhook_actions/config.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from .utils.config_file_args import ConfigFileArgs
+from tealprint import TealLevel, TealPrint
+
+from .core.entities.config_file_args import ConfigFileArgs
 
 _app_name = "webhook-actions"
 
@@ -10,17 +12,16 @@ class Config:
         # Default values
         self.app_name: str = _app_name
         self.webhook_dir = Path.home().joinpath(self.app_name)
-        self.debug: bool = False
-        self.verbose: bool = False
+        self.log_level: TealLevel = TealLevel.info
         self.port: int = 5000
 
     def set_args_settings(self, args):
         """Set additional configuration from script arguments"""
-        self.verbose = args.verbose
-        self.debug = args.debug
-
-        if args.debug:
-            self.verbose = True
+        if args.verbose:
+            self.log_level = TealLevel.verbose
+        elif args.debug:
+            self.log_level = TealLevel.debug
+        TealPrint.level = self.log_level
 
     def set_from_config_file(self, args: ConfigFileArgs) -> None:
         if args.port:
